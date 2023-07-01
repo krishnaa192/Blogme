1769299b565de0a6e632218d689a2d06b5b6147b
Krishna Upadhyay <86311851+krishnaa192@users.noreply.github.com>
2023-07-02 00:38:36 +0530

    Update urls.py

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 # app_name='blogging'
 
 urlpatterns = [
@@ -16,3 +17,5 @@ urlpatterns = [
 
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
